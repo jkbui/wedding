d0ff88d875350308df613b7529c9a7c592a3a63c
June Bui <kieuvanbui1402@gmail.com>
2023-03-10 17:48:26 +0100

    add react-app

---
+++ wedding-frontend/src/App.js
@@ -0,0 +1,14 @@
+import 'antd/dist/reset.css';
+import './App.css';
+import {Button} from 'antd';
+import { Card } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Card><h1>We are getting married</h1></Card>
+    </div>
+  );
+}
+
+export default App;
